781b703a80e2233903230af74ca08d31e701506c
Bruno Passos <brpassos.programador@gmail.com>
2022-08-25 20:38:58 -0300

    Feature 08 - Primeiro contato com leitura de arquivo

--- applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/model/test/AppImpressao.java
+++ applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/model/test/AppImpressao.java
@@ -3,10 +3,45 @@ package br.edu.infnet.applocacaoequipamento.model.test;
 import br.edu.infnet.applocacaoequipamento.interfaces.IPrinter;
 import br.edu.infnet.applocacaoequipamento.model.domain.Equipamento;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class AppImpressao {
 
     public  static void relatorio(String mensagem, IPrinter printer) {
         System.out.println(mensagem);
         printer.impressao();
+    }
+
+    public static void main(String[] args) {
+        String dir = "/aula_java/";
+        String arq = "locacoes.txt";
+
+        try{
+            try {
+                FileReader fileReader = new FileReader(dir+arq);
+                BufferedReader leitura = new BufferedReader(fileReader);
+
+                String linha = leitura.readLine();
+                while (linha != null){
+                    System.out.println(linha);
+
+                    linha = leitura.readLine();
+                }
+
+                leitura.close();
+                fileReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("[ERRO] O arquivo não existe!!!");
+            } catch (IOException e) {
+                System.out.println("[ERRO] Problema no fechamento do arquivo!!!");
+            }
+        }finally {
+            System.out.println("Terminou!!!");
+        }
+
+
     }
 }
