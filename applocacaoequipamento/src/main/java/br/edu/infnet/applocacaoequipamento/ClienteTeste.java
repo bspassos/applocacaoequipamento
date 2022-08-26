7d413872f650f06e8f5c42ee02713bea7ec886ad
Bruno Passos <brpassos.programador@gmail.com>
2022-08-26 20:00:33 -0300

    Feature 08 - Cliente - leitura por arquivo

--- applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/ClienteTeste.java
+++ applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/ClienteTeste.java
@@ -1,13 +1,25 @@
 package br.edu.infnet.applocacaoequipamento;
 
 import br.edu.infnet.applocacaoequipamento.controller.ClienteController;
+import br.edu.infnet.applocacaoequipamento.controller.LocacaoController;
 import br.edu.infnet.applocacaoequipamento.model.domain.Cliente;
+import br.edu.infnet.applocacaoequipamento.model.domain.Equipamento;
+import br.edu.infnet.applocacaoequipamento.model.domain.Locacao;
+import br.edu.infnet.applocacaoequipamento.model.exception.ClienteNuloException;
 import br.edu.infnet.applocacaoequipamento.model.exception.CpfInvalidoException;
+import br.edu.infnet.applocacaoequipamento.model.exception.LocacaoSemEquipamentoException;
 import br.edu.infnet.applocacaoequipamento.model.test.AppImpressao;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @Order(1)
@@ -19,39 +31,37 @@ public class ClienteTeste implements ApplicationRunner {
         System.out.println("===================================================");
         System.out.println("######cliente");
 
-        try {
-            Cliente c1 = new Cliente("Pedro", "12345678900", "pedro@nobarquinho.com");
-            ClienteController.incluir(c1);
-        } catch (CpfInvalidoException e) {
-            System.out.println("[ERROR - CLIENTE] " + e.getMessage());
-        }
-
-        try {
-            Cliente c2 = new Cliente("Thiago", "78945612399", "thiago@nobarquinho.com");
-            ClienteController.incluir(c2);
-        } catch (CpfInvalidoException e) {
-            System.out.println("[ERROR - CLIENTE] " + e.getMessage());
-        }
-
-        try {
-            Cliente c3 = new Cliente("Joao", "32165498700", "joao@nobarquinho.com");
-            ClienteController.incluir(c3);
-        } catch (CpfInvalidoException e) {
-            System.out.println("[ERROR - CLIENTE] " + e.getMessage());
-        }
+        String dir = "/aula_java/";
+        String arq = "clientes.txt";
 
         try{
-            Cliente c4 = new Cliente("CPF Nulo", null, "cpfnulo@foradobarquinho.com");
-            ClienteController.incluir(c4);
-        } catch (CpfInvalidoException e) {
-            System.out.println("[ERROR - CLIENTE] " + e.getMessage());
-        }
+            try {
+                FileReader fileReader = new FileReader(dir+arq);
+                BufferedReader leitura = new BufferedReader(fileReader);
 
-        try{
-            Cliente c5 = new Cliente("CPF vazio", "", "cpfvazio@foradobarquinho.com");
-            ClienteController.incluir(c5);
-        } catch (CpfInvalidoException e) {
-            System.out.println("[ERROR - CLIENTE] " + e.getMessage());
+                String linha = leitura.readLine();
+                while (linha != null){
+
+                    try {
+                        String[] campos = linha.split(";");
+                        Cliente c1 = new Cliente(campos[0], campos[1], campos[2]);
+                        ClienteController.incluir(c1);
+                    } catch (CpfInvalidoException e) {
+                        System.out.println("[ERROR - CLIENTE] " + e.getMessage());
+                    }
+
+                    linha = leitura.readLine();
+                }
+
+                leitura.close();
+                fileReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("[ERRO] O arquivo não existe!!!");
+            } catch (IOException e) {
+                System.out.println("[ERRO] Problema no fechamento do arquivo!!!");
+            }
+        }finally {
+            System.out.println("Terminou!!!");
         }
 
         System.out.println("===================================================");
