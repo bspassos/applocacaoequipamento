ff32b206a7b7d6db0f088aaed62706ecb8f19ce9
Bruno Passos <brpassos.programador@gmail.com>
2022-09-05 21:14:02 -0300

    Java Web - Feature 03 - Atuar no processo de logout

--- applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/controller/AppController.java
+++ applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/controller/AppController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.http.HttpSession;
 
 @SessionAttributes("user")
 @Controller
@@ -43,8 +46,10 @@ public class AppController {
     }
 
     @GetMapping(value = "/logout")
-    public String logout(Model model){
-        model.addAttribute("user", "");
+    public String logout(HttpSession session, SessionStatus status){
+
+        status.setComplete();
+        session.removeAttribute("user");
 
         return "redirect:/";
     }
