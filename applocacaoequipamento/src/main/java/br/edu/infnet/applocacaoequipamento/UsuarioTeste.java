17138420c42f34a6cd544e244ebbac5eb574b9a9
Bruno Passos <brpassos.programador@gmail.com>
2022-09-05 21:45:36 -0300

    Java Web - Feature 03 - Agora vamos aumentar o nosso cadastro de usuário

--- applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/UsuarioTeste.java
+++ applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/UsuarioTeste.java
@@ -1,6 +1,9 @@
 package br.edu.infnet.applocacaoequipamento;
 
+import br.edu.infnet.applocacaoequipamento.controller.ClienteController;
+import br.edu.infnet.applocacaoequipamento.model.domain.Cliente;
 import br.edu.infnet.applocacaoequipamento.model.domain.Usuario;
+import br.edu.infnet.applocacaoequipamento.model.exception.CpfInvalidoException;
 import br.edu.infnet.applocacaoequipamento.model.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -19,6 +22,8 @@ public class UsuarioTeste implements ApplicationRunner {
 
     @Autowired
     UsuarioService usuarioService;
+
+    private Usuario usuario;
     @Override
     public void run(ApplicationArguments args) {
         System.out.println("===================================================");
@@ -30,6 +35,43 @@ public class UsuarioTeste implements ApplicationRunner {
         usuario.setSenha("123");
 
         usuarioService.incluir(usuario);
+
+        String dir = "/aula_java/";
+        String arq = "usuarios.txt";
+
+        try{
+            try {
+                FileReader fileReader = new FileReader(dir+arq);
+                BufferedReader leitura = new BufferedReader(fileReader);
+
+                String linha = leitura.readLine();
+                while (linha != null){
+
+                    String[] campos = linha.split(";");
+
+                    System.out.println(campos[0]);
+
+                    usuario = new Usuario();
+                    usuario.setEmail(campos[0]);
+                    usuario.setNome(campos[1]);
+                    usuario.setSenha(campos[2]);
+
+                    usuarioService.incluir(usuario);
+
+                    linha = leitura.readLine();
+                }
+
+                leitura.close();
+                fileReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("[ERRO] O arquivo não existe!!!");
+            } catch (IOException e) {
+                System.out.println("[ERRO] Problema no fechamento do arquivo!!!");
+            }
+        }finally {
+            System.out.println("Terminou!!!");
+        }
+
 
         System.out.println("===================================================");
 
