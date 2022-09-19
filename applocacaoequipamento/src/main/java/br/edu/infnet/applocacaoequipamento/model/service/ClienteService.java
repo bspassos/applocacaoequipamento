1b570c615800602fed82c6c13c186e935c8a5cdb
Bruno Passos <brpassos.programador@gmail.com>
2022-09-19 20:24:34 -0300

    Java Web - Feature 06 - Banco de dados e a camada de repository (finalizado)

--- applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/model/service/ClienteService.java
+++ applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/model/service/ClienteService.java
@@ -1,32 +1,32 @@
 package br.edu.infnet.applocacaoequipamento.model.service;
 
 import br.edu.infnet.applocacaoequipamento.model.domain.Cliente;
+import br.edu.infnet.applocacaoequipamento.model.repository.ClienteRepository;
 import br.edu.infnet.applocacaoequipamento.model.test.AppImpressao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class ClienteService {
 
-    private static Map<Integer, Cliente> mapaCliente = new HashMap<Integer, Cliente>();
-    private static Integer id = 1;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     public void incluir(Cliente cliente){
-        cliente.setId(id++);
-        mapaCliente.put(cliente.getId(), cliente);
+
+        clienteRepository.save(cliente);
 
         AppImpressao.relatorio("Cadastro do cliente " + cliente.getNome() + " realizado com sucesso!", cliente);
     }
 
     public Collection<Cliente> obterLista(){
-        return mapaCliente.values();
+        return (Collection<Cliente>) clienteRepository.findAll();
     }
 
     public void excluir(Integer id){
-        mapaCliente.remove(id);
+        clienteRepository.deleteById(id);
     }
 
 }
