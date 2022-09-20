0795e7a9a049d3d4219099c98d21f25709226055
Bruno Passos <brpassos.programador@gmail.com>
2022-09-20 17:49:33 -0300

    Java Web - listagem dinamica de equipamentos

--- applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/controller/EquipamentoController.java
+++ applocacaoequipamento/src/main/java/br/edu/infnet/applocacaoequipamento/controller/EquipamentoController.java
@@ -1,13 +1,22 @@
 package br.edu.infnet.applocacaoequipamento.controller;
 
+import br.edu.infnet.applocacaoequipamento.model.service.EquipamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EquipamentoController {
 
+    @Autowired
+    private EquipamentoService equipamentoService;
+
     @GetMapping(value = "/equipamento/lista")
-    public String telaLista() {
+    public String telaLista(Model model) {
+
+        model.addAttribute("listagem", equipamentoService.obterLista());
+
         return "equipamento/lista";
     }
 }
